07b1654df5c26ef4586f765931254672260ef7db
DA COSTA Lucas <ldacosta7797@gmail.com>
2023-03-09 00:30:26 +0100

    ajout de la page 3

--- app/src/main/java/com/example/myenquetedisneyland/Page2.java
+++ app/src/main/java/com/example/myenquetedisneyland/Page2.java
@@ -2,13 +2,34 @@ package com.example.myenquetedisneyland;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
-public class Page2 extends AppCompatActivity {
+public class Page2 extends AppCompatActivity implements View.OnClickListener {
+
+    private RadioGroup rgEntretien, rgAttente ;
+    private Button btSuivant ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
+
+        this.rgEntretien = findViewById(R.id.idEntretien);
+        this.rgAttente = findViewById(R.id.idAttente);
+        this.btSuivant = findViewById(R.id.idSuivant2);
+
+        this.btSuivant.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()== R.id.idSuivant2){
+            Intent unIntent = new Intent(this, Page3.class);
+            this.startActivity(unIntent);
+        }
     }
 }
