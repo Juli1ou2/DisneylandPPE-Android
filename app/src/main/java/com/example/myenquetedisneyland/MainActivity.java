b28019195cc85ab77d69af4000b00bef82d12640
DA COSTA Lucas <ldacosta7797@gmail.com>
2023-03-08 17:55:32 +0100

    ajout de l'accueil de l'app et de l'activity Inscription pour tester le changement d'activity

--- app/src/main/java/com/example/myenquetedisneyland/MainActivity.java
+++ app/src/main/java/com/example/myenquetedisneyland/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.myenquetedisneyland;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btInscritpion;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //faire le lien entre objet prog et graphique
+        this.btInscritpion = findViewById(R.id.idInscription);
+        //rendre les boutons ecoutables
+        this.btInscritpion.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()== R.id.idInscription){
+            Intent unIntent = new Intent(this, Inscription.class);
+            this.startActivity(unIntent);
+        }
     }
 }
