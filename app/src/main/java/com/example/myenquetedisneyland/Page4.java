7241746c631693183e8ff892ac3e545a4b172423
DA COSTA Lucas <ldacosta7797@gmail.com>
2023-03-10 12:28:58 +0100

    ajout de la custom ratebar

--- app/src/main/java/com/example/myenquetedisneyland/Page4.java
+++ app/src/main/java/com/example/myenquetedisneyland/Page4.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.RatingBar.OnRatingBarChangeListener;
 import android.widget.Toast;
 
 public class Page4 extends AppCompatActivity implements View.OnClickListener {
     private Button btFin ;
+   RatingBar rbMickey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,19 @@ public class Page4 extends AppCompatActivity implements View.OnClickListener {
         setContentView(R.layout.activity_page4);
 
         this.btFin = findViewById(R.id.idFin);
+        rbMickey=(RatingBar)findViewById(R.id.idNote);
+
+        rbMickey.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener(){
+
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating,
+                                        boolean fromUser) {
+                // TODO Auto-generated method stub
+                Toast.makeText(getApplicationContext(),Float.toString(rating),Toast.LENGTH_LONG).show();
+
+            }
+
+        });
 
         this.btFin.setOnClickListener(this);
     }
